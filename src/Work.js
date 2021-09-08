0ca0a484930627c8b7468b7d803d3667964b829d
johnsonthieu <johnsonle2929@gmail.com>
2021-09-08 13:13:01 -0700

    Adds vergil app work

--- src/Work.js
+++ src/Work.js
@@ -6,12 +6,21 @@ class Work extends Component{
     return(
       <div className="work container">
         <h3><b>WORK EXPERIENCE</b></h3>
+        <h4>Junior Frontend Software Engineer, Contract Role <a href="https://www.vergil.app/"> Vergil App </a> New York, NY, March 2021 - July 2021</h4>
+          <ul>
+            <li>Collaborate with stakeholders and Lead Engineers regarding design, structure, functionality and key components required</li>
+            <li></li>Built React components and screens for app and storybook
+            <li></li>Built frontend theme design with css
+            <li></li>Manage Github Repo, completed sprint assigned issues, Opening New Issues and Closing Pull Request with detailed summary
+          </ul>
+
           <h4>Product Engineering Intern, <a href="https://www.frontdoorhome.com/">Frontdoor Inc.</a> Denver, CO, June 2020 - September 2020</h4>
           <ul>
             <li>Collaborating with colleagues from multidisciplinary science and engineering</li>
             <li>Building components for company component-library using React.js</li>
             <li>Writing documents of component design and dashboard to analyze key inputs to internal systems</li>
           </ul>
+
           <h4>Candidate Experience Representative, <a href="www.checker.com">Checkr Inc.</a> San Francisco, February 2018 - January 2020 </h4>
           <ul>
             <li>Utilized Checkr Dashboard, Zendesk, and Looker providing a 90% above quality average. Assist candidate callers with their background check reports, to produce a “5 star candidate experience”.</li>
