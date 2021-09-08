674166e75040b45c0967ca892a166cefc21d9ca1
johnsonthieu <johnsonle2929@gmail.com>
2021-09-08 15:37:18 -0700

    Updates

---
+++ precache-manifest.f9bba531c8aea7b33eefac2f1f866165.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c881f985c26b0ad463b3ec8a14568fe",
+    "url": "/gh-pages-resume/index.html"
+  },
+  {
+    "revision": "959d959dbcebb9abf9e6",
+    "url": "/gh-pages-resume/static/css/2.ff29d27e.chunk.css"
+  },
+  {
+    "revision": "0575e0791d85f216245e",
+    "url": "/gh-pages-resume/static/css/main.0e283e73.chunk.css"
+  },
+  {
+    "revision": "959d959dbcebb9abf9e6",
+    "url": "/gh-pages-resume/static/js/2.e7eef72a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-resume/static/js/2.e7eef72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0575e0791d85f216245e",
+    "url": "/gh-pages-resume/static/js/main.bfbc2622.chunk.js"
+  },
+  {
+    "revision": "c9ebacf6d794093bc1b4",
+    "url": "/gh-pages-resume/static/js/runtime-main.d0f7c7bb.js"
+  },
+  {
+    "revision": "6b2e640a503c232b37aa44c39f8550ec",
+    "url": "/gh-pages-resume/static/media/clubhoppin.6b2e640a.mov"
+  },
+  {
+    "revision": "3ad21711c4cc437c50f4b75a43922809",
+    "url": "/gh-pages-resume/static/media/diceracer.3ad21711.mov"
+  },
+  {
+    "revision": "61aaa884f3f8ff50213cd802436bcea6",
+    "url": "/gh-pages-resume/static/media/headshot.61aaa884.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7507b5207219ac6ea41676a14675c5bb",
+    "url": "/gh-pages-resume/static/media/pikatsume.7507b520.mov"
+  }
+]);
